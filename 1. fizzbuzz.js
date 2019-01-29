8fde79c50bd81b590c3299aa943b8c2c46febc28
Diljeet Kakkar <30684526+diljeetk@users.noreply.github.com>
2019-01-29 12:07:40 +0530

    updated description
    for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.

--- 1. fizzbuzz.js
+++ 1. fizzbuzz.js
@@ -1,3 +1,6 @@
+// FizzBuzz : - for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+
 function FizzBuzz(num) {
     for(var i = 1; i<=num; i++) {
         if(i%3 === 0 && i%5 === 0) console.log('fizzbuzz');
